74efee0255592bfdc2ce903a7e898e3655a9345c
Avinash Kumar <itsmeatg@outlook.com>
2021-10-01 07:46:32 +0530

    Adding Header

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+
+const App = ( ) => (
+  
+    <div>
+      <Header/>
+      <p>App</p>
+    </div>
+  
+);
+
+export default App;
