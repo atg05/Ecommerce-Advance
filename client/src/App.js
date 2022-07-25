23c9370236f4892cd0dbc88b55f9418c93cf3040
Avinash Kumar <itsmeatg@outlook.com>
2022-07-25 08:06:31 +0530

    Finishing category icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Homepage from "./containers/homepage/Homepage";
 import Navbar from "./containers/navbar/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { loggedIn } from "./redux/slice/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
+
   return (
     <>
       <Navbar />
+      <button onClick={() => dispatch(loggedIn())}>Login</button>
+      <Homepage />
     </>
   );
 }
